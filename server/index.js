769b4f9945e3df6baab672d2f56daa92d5c689a6
Ruy Adorno <radorno@upgrade.com>
2018-09-07 20:10:13 -0400

    Add mock server (#3)
    * Added mock server
    
    * Added server tests
    
    * Added parallel running mock and dev servers

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/api/colors', (req, res) => setTimeout(() => res.json([
+  'black',
+  'blue',
+  'green',
+  'red',
+  'white'
+]), 3000 * Math.random()));
+
+app.post('/api/submit', express.json(), (req, res) => setTimeout(() => {
+  const { name, email, password, color, terms } = req.body;
+  if (name && email && password && color && terms && name !== 'Error') {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(400);
+  }
+}, 3000 * Math.random()));
+
+if (!module.parent) {
+  app.listen(3001, () => console.log('Mock server running'));
+}
+
+module.exports = app;
