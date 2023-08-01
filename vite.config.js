2103f20d25e1cfa0a1ed185d1da572ef52a0c995
Gabriel Bédard Sicé <gbedardsice@upgrade.com>
2023-08-01 15:55:26 -0400

    Fix CORS issue with vite server

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dns from "dns";
+
+// Display localhost instead of 127.0.0.1 for CORS purposes.
+// See [this](https://vitejs.dev/config/server-options.html#server-host).
+dns.setDefaultResultOrder("verbatim");
 
 export default defineConfig(() => {
   return {
     build: {
       outDir: "build",
+    },
+    server: {
+      port: 3000,
     },
     plugins: [react()],
   };
